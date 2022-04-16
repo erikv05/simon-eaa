3cb418e20a8e1f4bfba488e520e8c8bb877a5812
Albert Luo <albertluo040605@gmail.com>
2022-04-15 22:25:21 -0700

    Added player input functionality, game over when click the wrong sequence, and playability.

--- app/src/main/java/org/overlake/mat803/simon/GameFragment.java
+++ app/src/main/java/org/overlake/mat803/simon/GameFragment.java
@@ -3,6 +3,7 @@ package org.overlake.mat803.simon;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import org.overlake.mat803.simon.databinding.FragmentGameBinding;
 
@@ -26,11 +28,17 @@ public class GameFragment extends Fragment {
     private ArrayList<Button> mSequence;
     private int mSequenceLength;
     private EnumMap<Button, Integer> mSounds;
+    private boolean mPlaying;
+    private boolean mStart;
+    private int currIndex;
 
     public GameFragment() {
         mSequence = new ArrayList<>();
         mSounds = new EnumMap<Button, Integer>(Button.class);
         mSequenceLength = 0;
+        mPlaying = true;
+        mStart = false;
+        currIndex = 0;
     }
 
     @Override
@@ -42,15 +50,17 @@ public class GameFragment extends Fragment {
         mSounds.put(Button.BLUE, mSoundPool.load(getContext(),R.raw.tone209,1));
 
         //i incremented this by 5 just for a test
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
             incrementSequence();
         }
+        FragmentGameBinding binding = FragmentGameBinding.inflate(inflater);
 
         CountDownTimer timer = new CountDownTimer(1000L * mSequenceLength,1000) {
-
             int index = 0;
             @Override
             public void onTick(long l) {
+                mPlaying = false;
+                mStart = false;
                 mSoundPool.play(mSounds.get(mSequence.get(index)),1.0f,1.0f,1,0, 1.0f);
                 changeColor(mSequence.get(index));
                 index++;
@@ -58,20 +68,127 @@ public class GameFragment extends Fragment {
 
             @Override
             public void onFinish() {
+                mPlaying = true;
+                mStart = true;
                 index = 0;
             }
         };
-        FragmentGameBinding binding = FragmentGameBinding.inflate(inflater);
+
         binding.play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(mPlaying) {
                     timer.start();
+                }
             }
         });
+
+        binding.green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mStart) {
+                    int i = getIndex();
+                    buttonCalled(Button.GREEN, i);
+                    if(i+1>=mSequenceLength){
+                        replay(timer);
+                    }
+                }
+            }
+        });
+        binding.blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mStart) {
+                        int i = getIndex();
+                        buttonCalled(Button.BLUE, i);
+                        if(i+1>=mSequenceLength){
+                            replay(timer);
+                        }
+                }
+            }
+        });
+        binding.yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mStart) {
+                    int i = getIndex();
+                    buttonCalled(Button.YELLOW, i);
+                    if(i+1>=mSequenceLength){
+                        replay(timer);
+                    }
+                }
+            }
+        });
+        binding.red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mStart) {
+                    int i = getIndex();
+                    buttonCalled(Button.RED, i);
+                    if(i+1>=mSequenceLength){
+                        replay(timer);
+                    }
+                }
+            }
+        });
+
 
         return binding.getRoot();
     }
 
+    private void addIndex(){
+        currIndex++;
+    }
+
+    private void resetIndex(){
+        currIndex = 0;
+    }
+
+    private int getIndex(){
+        return currIndex;
+    }
+
+    private void gameOver(){
+        NavHostFragment.findNavController(this).navigate(R.id.action_gameFragment2_to_endScreen);
+    }
+
+    private void buttonCalled(Button b, int i){
+        mSoundPool.play(mSounds.get(b), 1.0f, 1.0f, 1, 0, 1.0f);
+        changeColor(b);
+        if(b==mSequence.get(i)){
+            addIndex();
+        }else{
+            gameOver();
+        }
+    }
+
+    private void replay(CountDownTimer timer1){
+        timer1.cancel();
+        incrementSequence();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                CountDownTimer timer = new CountDownTimer(1000L * mSequenceLength,1000) {
+                    int index = 0;
+                    @Override
+                    public void onTick(long l) {
+                        mPlaying = false;
+                        mSoundPool.play(mSounds.get(mSequence.get(index)),1.0f,1.0f,1,0, 1.0f);
+                        changeColor(mSequence.get(index));
+                        index++;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        mPlaying = true;
+                        index = 0;
+                    }
+                };
+                resetIndex();
+                timer.start();
+            }
+        },1000);
+    }
     private void incrementSequence() {
             mSequence.add(Button.getRandomButton());
             mSequenceLength++;
@@ -127,4 +244,5 @@ public class GameFragment extends Fragment {
         };
         timer.start();
     }
+
 }
